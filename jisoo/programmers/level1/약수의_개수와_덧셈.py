8a8612d609afd20ee7dc72d0270c0be1876628d9
변지수 <bjs1616@gmail.com>
2022-11-19 23:14:58 +0900

    9일차_프로그래머스

---
+++ jisoo/programmers/level1/약수의_개수와_덧셈.py
@@ -0,0 +1,34 @@
+"""
+https://school.programmers.co.kr/learn/courses/30/lessons/77884
+"""
+
+
+def counting(i):
+    count = 0
+    for x in range(1, i + 1):
+        if i % x == 0:
+            count += 1
+    return count
+
+
+def solution(left, right):
+    answer = 0
+
+    for i in range(left, right + 1):
+        count_ = counting(i)
+        if count_ % 2 == 0:
+            answer += i
+        else:
+            answer -= i
+
+    return answer
+
+# 단축풀이
+def solution2(left, right):
+    answer = 0
+    for i in range(left,right+1):
+        if int(i**0.5)==i**0.5:
+            answer -= i
+        else:
+            answer += i
+    return answer
