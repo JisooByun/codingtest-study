e8c9b97701a4d90a3699e23516a83e928ed6521a
변지수 <bjs1616@gmail.com>
2022-11-23 18:52:08 +0900

    11일차_프로그래머스

---
+++ jisoo/programmers/level1/음양_더하기.py
@@ -0,0 +1,13 @@
+"""
+https://school.programmers.co.kr/learn/courses/30/lessons/76501
+"""
+def solution(absolutes, signs):
+    answer = 0
+
+    for a,b in zip(absolutes,signs):
+        if b:
+            answer +=a
+        else:
+            answer -=a
+
+    return answer
